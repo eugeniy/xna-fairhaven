93ecc21d7b51d63e3e0ad9cc062001448c21d75b
Koteako <info@koteako.com>
2011-04-20 14:26:32 -0700

    Initial import

---
+++ TowerDefense/TowerDefense/Engine/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefense
+{
+    public class Cell
+    {
+        public Cell()
+        {
+        }
+    }
+}
---
+++ TowerDefense/TowerDefense/Engine/Grid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefense
+{
+    public class Grid
+    {
+        protected List<Cell> grid;
+
+        public Grid(int width, int height)
+        {
+        }
+    }
+}
